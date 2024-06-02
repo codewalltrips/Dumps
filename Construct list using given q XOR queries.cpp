da5088164bad138888690c63a473fa449bbf59a7
Triparna singha <153750421+codewalltrips@users.noreply.github.com>
2024-06-02 22:32:06 +0530

    Create Construct list using given q XOR queries.cpp
    dsa

---
+++ Construct list using given q XOR queries.cpp
@@ -0,0 +1,147 @@
+/*Given a list s that initially contains only a single value 0. There will be q queries of the following types:
+
+0 x: Insert x in the list
+1 x: For every element a in s, replace it with a ^ x. ('^' denotes the bitwise XOR operator)
+Return the sorted list after performing the given q queries.
+
+Example 1:
+
+Input:
+q = 5
+queries[] = {{0, 6}, {0, 3}, {0, 2}, {1, 4}, {1, 5}}
+Output:
+1 2 3 7
+Explanation:
+[0] (initial value)
+[0 6] (add 6 to list)
+[0 6 3] (add 3 to list)
+[0 6 3 2] (add 2 to list)
+[4 2 7 6] (XOR each element by 4)
+[1 7 2 3] (XOR each element by 5)
+The sorted list after performing all the queries is [1 2 3 7]. 
+Example 2:
+Input:
+q = 3
+queries[] = {{0, 2}, {1, 3}, {0, 5}} 
+Output :
+1 3 5
+Explanation:
+[0] (initial value)
+[0 2] (add 2 to list)
+[3 1] (XOR each element by 3)
+[3 1 5] (add 5 to list)
+The sorted list after performing all the queries is [1 3 5].
+
+Your Task:  
+You don't need to read input or print anything. Your task is to complete the function constructList() which takes an integer q the number of queries and queries[] a list of lists of length 2 denoting the queries as input parameters and returns the final constructed list.
+
+
+Expected Time Complexity: O(q*log(q))
+Expected Auxiliary Space: O(l), where l is only used for output-specific requirements.
+
+
+Constraints:
+1 ≤ q ≤ 105*/
+//{ Driver Code Starts
+#include <bits/stdc++.h>
+using namespace std;
+
+class Matrix {
+  public:
+    template <class T>
+    static void input(vector<vector<T>> &A, int n, int m) {
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < m; j++) {
+                scanf("%d ", &A[i][j]);
+            }
+        }
+    }
+
+    template <class T>
+    static void print(vector<vector<T>> &A) {
+        for (int i = 0; i < A.size(); i++) {
+            for (int j = 0; j < A[i].size(); j++) {
+                cout << A[i][j] << " ";
+            }
+            cout << endl;
+        }
+    }
+};
+
+class Array {
+  public:
+    template <class T>
+    static void input(vector<T> &A, int n) {
+        for (int i = 0; i < n; i++) {
+            scanf("%d ", &A[i]);
+        }
+    }
+
+    template <class T>
+    static void print(vector<T> &A) {
+        for (int i = 0; i < A.size(); i++) {
+            cout << A[i] << " ";
+        }
+        cout << endl;
+    }
+};
+
+
+// } Driver Code Ends
+
+
+
+class Solution {
+ 
+  public:
+    vector<int> constructList(int q, vector<vector<int>> &queries) {
+        // code here
+        
+        vector<int>ans ;
+        // ans.push_back(0) ;
+        int temp = 0 ;
+        for(int i = q-1 ; i>= 0 ; i--){
+            if(queries[i][0]==0){
+                ans.push_back(queries[i][1] ^ temp) ;
+            }
+            else{
+                temp ^= queries[i][1] ;
+            }
+        }
+        
+        ans.push_back(temp) ;
+        sort(ans.begin(),ans.end()) ;
+        
+        
+        return ans ;
+    }
+};
+
+
+
+
+
+
+
+
+//{ Driver Code Starts.
+
+int main() {
+    int t;
+    scanf("%d ", &t);
+    while (t--) {
+
+        int q;
+        scanf("%d", &q);
+
+        vector<vector<int>> queries(q, vector<int>(2));
+        Matrix::input(queries, q, 2);
+
+        Solution obj;
+        vector<int> res = obj.constructList(q, queries);
+
+        Array::print(res);
+    }
+}
+
+// } Driver Code Ends
